d6671219a6469658d96bd1425ea4cf24fd12a028
Kevin SeungWhan Chung <schung58@illinois.edu>
2016-07-29 15:36:53 -0500

    shell command for moving file from xpacc to local

---
+++ movefiles.sh
@@ -0,0 +1,3 @@
+scp -r $XPACC1:PIC/1d3v_pm_mcc_f90/data/rf_Ar4/\{input,record,E.bin,KE_1.bin,KE_2.bin,Ncoll.bin,Np.bin,PE.bin,phi.bin,rho.bin\} ~/Desktop/rf_Ar4/;
+scp -r $XPACC1:PIC/1d3v_pm_mcc_f90/data/rf_Ar4/xp/\{958_1.bin,958_2.bin\} ~/Desktop/rf_Ar4/xp/;
+scp -r $XPACC1:PIC/1d3v_pm_mcc_f90/data/rf_Ar4/vp/\{958_1.bin,958_2.bin\} ~/Desktop/rf_Ar4/vp/;
