30d78ee4948bbe4899208f263e176cc2f553e58f
Kevin SeungWhan Chung <schung58@illinois.edu>
2017-03-03 09:39:19 -0700

    debye sampling

--- main.f90
+++ main.f90
@@ -35,7 +35,8 @@ program main
 !	call RedistributionTest
 !	call updateWeightTest
 !	call debye_sensitivity_curve
-	call adjoint_convergence_in_time(debye_adj)
+!	call adjoint_convergence_in_time(debye_adj)
+	call debye_sampling
 
 	! print to screen
 	print *, 'program main...done.'
@@ -697,6 +698,84 @@ contains
 		   close(302)
 			close(303)
 		end if
+
+		call destroyMPIHandler(mpih)
+	end subroutine
+
+	subroutine debye_sampling
+		type(PM1D) :: d
+		type(adjoint) :: adj
+		type(FSens) :: fs
+		type(recordData) :: r,fsr
+		type(mpiHandler) :: mpih
+		integer, parameter  :: Nsample=1
+		real(mp), parameter :: vT=1.5_mp
+		integer, parameter :: N = 100000, Ng = 64
+		integer, parameter :: NInit=5E4, Ngv=32, NInject=5E3, NLimit=3E5
+		real(mp) :: L = 20.0_mp, Lv, Q = 2.0_mp
+		real(mp) :: dt=0.05_mp, dx
+		real(mp) :: Time(4), A(2)
+		real(mp) :: J,grad,adj_grad(1)
+		integer :: i,k
+		character(len=100)::dir,Time_str
+		Time = (/ 0.1_mp, 30.0_mp, 150.0_mp, 750.0_mp /)
+		A = (/ vT, 0.0_mp /)
+
+		call buildMPIHandler(mpih)
+		call allocateBuffer(Nsample,3,mpih)
+
+		call init_random_seed(mpih%my_rank)
+		do k=1,size(Time)
+			J = 0.0_mp
+			grad = 0.0_mp
+			adj_grad = 0.0_mp
+			do i=1,mpih%sendcnt
+				call buildPM1D(d,Time(k),0.0_mp,Ng,1,pBC=0,mBC=0,order=1,A=A,L=L,dt=dt)
+				dir = 'Debye_sampling/'//trim(adjustl(mpih%rank_str))
+				call buildRecord(r,d%nt,1,d%L,d%ng,trim(dir),20)
+
+				call buildSpecies(d%p(1),-1.0_mp,1.0_mp)
+				call Debye_initialize(d,N,Q)
+		
+				Lv = A(1)*6.0_mp
+				call buildFSens(fs,d,Lv,Ngv,NInject,NLimit)
+				dir = 'Debye_sampling/'//trim(adjustl(mpih%rank_str))//'/f_A'
+				call buildRecord(fsr,fs%dpm%nt,1,fs%dpm%L,fs%dpm%ng,trim(dir),20)
+				call Debye_sensitivity_init(fs,2*N,A(1))
+	
+				call forwardsweep_sensitivity(d,r,fs,fsr,Null_input,Null_source,Debye,J,grad)
+
+				call buildAdjoint(adj,d)
+				call adj%m%setMesh(d%m%rho_back)
+				call backward_sweep(adj,d,r,adj_grad,dDebye,Null_dinput,dDebye_dvT,Null_input,Null_source)
+
+				mpih%sendbuf(i,:) = (/J,grad,adj_grad(1)/)
+
+				call destroyRecord(r)
+				call destroyPM1D(d)
+				call destroyAdjoint(adj)
+				call destroyRecord(fsr)
+				call destroyFSens(fs)
+			end do
+
+			call gatherData(mpih)
+
+			if( mpih%my_rank.eq.mpih%size-1 ) then
+				write(Time_str,*) Time(k)
+				open(unit=301,file='data/Debye_sampling/Jk_'//trim(Time_str)//'.bin',	&
+						status='replace',form='unformatted',access='stream')
+				open(unit=302,file='data/Debye_sampling/gradk_'//trim(Time_str)//'.bin',	&
+						status='replace',form='unformatted',access='stream')
+				open(unit=303,file='data/Debye_sampling/adjk_'//trim(Time_str)//'.bin',	&
+						status='replace',form='unformatted',access='stream')
+			   write(301) mpih%recvbuf(:,1)
+			   write(302) mpih%recvbuf(:,2)
+				write(303) mpih%recvbuf(:,3)
+			   close(301)
+			   close(302)
+				close(303)
+			end if
+		end do
 
 		call destroyMPIHandler(mpih)
 	end subroutine
