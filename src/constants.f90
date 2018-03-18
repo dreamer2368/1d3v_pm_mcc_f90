8ebcc14aa68bfb7d0eecf993e5f649f4bc6b36a8
Seung Whan Chung <chung28@quartz1538.llnl.gov>
2018-03-17 21:36:32 -0700

    main: read option for printing simulation details

--- src/constants.f90
+++ src/constants.f90
@@ -8,7 +8,7 @@ module constants
 !	integer, parameter :: mp = SELECTED_REAL_KIND(33,4931)
 	real(mp), parameter :: pi = 4.0_mp*ATAN(1.0_mp)
 	complex(mp), parameter :: eye = (0.0_mp,1.0_mp)
-    logical, parameter :: print_pm_output = .false.
+    logical :: print_pm_output = .false.
 
 contains
 
